9610e4c6f42b19f6c4043901b04a4ac191b3e44a
yabbes <yabbaspost@gmail.com>
2017-08-10 23:57:59 +0200

    help and ^last working

--- bot.js
+++ bot.js
@@ -22,13 +22,19 @@ console.log("connected to ", config.channels, " on ", config.server);
 // Handling messages in room
 bot.addListener('message', function (from, to, message) {
     console.log(from + ' => ' + to + ': ' + message);
-    messageAction.addToArchive(message, from);
+    messageAction.addToArchive(message, from); // log
     if(message.toUpperCase().startsWith(config.botName.toUpperCase())){
         bot.say(to, messageAction.react(message));
-    } else if (message.toUpperCase().startsWith('^rand'.toUpperCase())) {
+    } else if (message.toUpperCase().startsWith('^rand'.toUpperCase())) { // ^rand
         var split_m = message.split(' '); // nick = split_m[1]
         bot.say(to, split_m[1] + ": " + messageAction.rand(split_m[1]));
-    }
+    } else if (message.toUpperCase().startsWith('^last'.toUpperCase())) { // ^last
+        var split_m = message.split(' '); // nick = split_m[1]
+        bot.say(to, "vu la dernière fois " + split_m[1] + ": " + messageAction.last(split_m[1]));
+    } else if (message.toUpperCase().startsWith('^help'.toUpperCase())) { // ^help
+        var split_m = message.split(' '); // nick = split_m[1]
+        bot.say(to, from + ": " + messageAction.help());
+    } 
     
 
 });
