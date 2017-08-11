a289ed207750e13db698746419a8cd042874a139
yabbes <yabbaspost@gmail.com>
2017-08-11 12:03:33 +0200

    release 1.1

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var irc = require('irc');
 var weather = require('weather-js');
 var messageAction = require('./message_action');
 var config = {
-    channels: ["##francophonie"],
+    channels: ["##channel"],
     server: "irc.freenode.org",
     botName: "yabbot",
     autoRejoin: false,
